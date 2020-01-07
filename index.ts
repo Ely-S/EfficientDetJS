fd93dda1bddb78d443d04640ac3a56c146c2c374
Eli Sakov <eli.sakov@hotmail.com>
2020-01-07 13:31:49 -0800

    attempt on new model

--- index.ts
+++ index.ts
@@ -31,12 +31,10 @@ tf.serialization.registerClass(FixedDropout);
 // There is an operation in EfficientDet which uses a 
 // version of keras.dropout with a bug fixed. It is called FixedDropout
 
-
-
 const camConfig = {
     // facingMode: 'user', //'environment'
-    resizeWidth: 512,
-    resizeHeight: 512,
+    resizeWidth: 416,
+    resizeHeight: 416,
     centerCrop: true
 }
 
@@ -45,7 +43,6 @@ const canvasElement = <HTMLCanvasElement>  document.getElementById('canvas');
 
 videoElement.width = 640;
 videoElement.height = 480;
-
 
 canvasElement.width = 512;
 canvasElement.height = 512;
@@ -57,16 +54,52 @@ async function capturePhoto(){
     let img = await cam.capture();
 
     let scaledImage = img.div(tf.scalar(255))
-    tf.browser.toPixels(scaledImage, canvasElement)
+    tf.browser.toPixels(scaledImage, canvasElement) 
+    return scaledImage
 }
 
+async function startLoop(model){
+    let image = await capturePhoto()
+    model.predict(image)
+}
 
 capturePhoto()
 
+class Lambda extends tf.layers.Layer {
+    constructor() {
+      super({})
+    }
+  
+    static get className() {
+      return 'Lambda';
+    }
+  
+     call(inputs, kwargs) {
+      let input = inputs;
+      if (Array.isArray(input)) {
+        input = input[0];
+      }
+      this.invokeCallHook(inputs, kwargs);
+      /*const origShape = input.shape;
+      const flatShape =
+          [origShape[0], origShape[1] * origShape[2] * origShape[3]];
+      const flattened = input.reshape(flatShape);
+      const centered = tf.sub(flattened, flattened.mean(1).expandDims(1));
+      const pos = centered.relu().reshape(origShape);
+      const neg = centered.neg().relu().reshape(origShape);
+      return tf.concat([pos, neg], 3); */
+      return input.pow(tf.tensor(2).toInt())
+    }
+}
+  
+tf.serialization.SerializationMap.register(Lambda);
 
-const loadModelPromise =  tf.loadLayersModel('/model/model.json');
+const loadModelPromise =  tf.loadLayersModel('/model2/model.json');
 
-
+loadModelPromise.then(model => {
+    model.summary()
+    startLoop(model)
+})
 
 // const example = tf.fromPixels(webcamElement);  // for example
 // const prediction = model.predict(example);
