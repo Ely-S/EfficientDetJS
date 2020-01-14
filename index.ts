eb95968d881794badb9c5051c2d3dc6b24461509
Eli Sakov <eli.sakov@hotmail.com>
2020-01-14 12:33:52 -0800

    make it predict and draw People!

--- index.ts
+++ index.ts
@@ -42,17 +42,54 @@ canvasElement.height = 640;
 
 const camera = tf.data.webcam(videoElement, camConfig);
 
-async function capturePhoto() {
+async function capturePhoto(): Promise<Tensor3D> {
   let cam = await camera;
   let img = await cam.capture();
 
   let scaledImage = img.div(tf.scalar(255)) as Tensor3D
   tf.browser.toPixels(scaledImage, canvasElement)
+
+  return scaledImage
 }
 
+function drawBoxes(predictions, canvas) {
+  const ctx = canvas.getContext("2d");
+  // ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-capturePhoto()
+  const font = "24px helvetica";
+  ctx.font = font;
+  ctx.textBaseline = "top";
 
+  predictions.forEach(prediction => {
+    const x = prediction.bbox[0];
+    const y = prediction.bbox[1];
+    const width = prediction.bbox[2] - x;
+    const height = prediction.bbox[3] - y;
+
+    console.log(x, y, height, width)
+
+    // Draw the bounding box.
+    ctx.strokeStyle = "#FF0000";
+    ctx.lineWidth = 3;
+    ctx.strokeRect(x, y, width, height);
+
+    // Draw the label background.
+    ctx.fillStyle = "#FF0000";
+    const textWidth = ctx.measureText(prediction.class).width;
+    const textHeight = parseInt(font, 10);
+
+    // draw top left rectangle
+    ctx.fillRect(x, y, textWidth + 10, textHeight + 10);
+
+    // draw bottom left rectangle
+    ctx.fillRect(x, y + height - textHeight, textWidth + 15, textHeight + 10);
+
+    // Draw the text last to ensure  it's on top.
+    ctx.fillStyle = "#ffff";
+    ctx.fillText(prediction.class, x, y);
+    ctx.fillText(prediction.score.toFixed(2), x, y + height - textHeight);
+  });
+};
 
 async function start() {
   await tf.ready()
@@ -63,10 +100,8 @@ async function start() {
 
   model.summary()
 
-  let cam = await camera;
-  let img = await cam.capture();
 
-  let scaledImage = img.div(tf.scalar(255)) as Tensor3D
+  let scaledImage = await capturePhoto() as Tensor3D
   let batch = scaledImage.expandDims()
 
   console.log('DONE LOADING')
@@ -75,12 +110,82 @@ async function start() {
 
   console.log('done predicting')
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 2; i++) {
     console.time("Prediction")
-    model.predict([batch])
+    let p = model.predict([batch])
     console.timeEnd("Prediction")
+
+    let allBoxes = p[0]
+    let allScores = p[1]
+
+    // allBoxes has shape (batch_size, boxes, 4)
+    // allScores has shape (batch_size, boxes, n_classes)
+    // where boxes = n_ancor_boxes
+
+    // perform inference on one image at a time
+
+    let boxes = allBoxes.gather(0).squeeze()
+    let scores = allScores.gather(0).squeeze()
+
+
+    let maxOutPutSize = 2
+    let iouThreshold = .5
+    let scoreThreshold = .5
+
+    console.log(boxes)
+
+
+    // person is class 14
+    console.time("nms")
+
+    let scores_for_class_i = scores.$(":," + 14)
+
+    let indecies = await tf.image.nonMaxSuppressionAsync(
+      boxes, scores_for_class_i,
+      maxOutPutSize, iouThreshold, scoreThreshold
+    )
+
+    indecies.print()
+    let pickedBoxes = boxes.gather(indecies)
+    pickedBoxes.print()
+
+    let predBoxes = pickedBoxes.dataSync()
+    let predScores = scores_for_class_i.gather(indecies).dataSync()
+
+    console.log(predBoxes)
+    console.log(predScores)
+
+    console.timeEnd("nms")
+
+    for (let i = 0; i < predScores.length; i++) {
+      drawBoxes([{
+        bbox: predBoxes.slice(i * 4, i * 4 + 4),
+        class: "human",
+        score: predScores[i]
+      }], canvasElement)
+    }
+
+    console.log("draw end")
+
+
   }
 
+  // function nms_by_class(){
+  //   for (let i = 0; i < n_classes; i++) {
+  //     // let scores_for_class_i = scores.slice([0, i], [-1, i + 1]).squeeze()
+  //     let scores_for_class_i = scores.$(":," + i)
+
+  //     //@TODO: use nonMaxSuppressionWithScoreAsync
+  //     indecesPromises[i] = tf.image.nonMaxSuppressionAsync(
+  //       boxes, scores_for_class_i,
+  //       maxOutPutSize, iouThreshold, scoreThreshold
+  //     )
+
+  //   }
+
+  // }
+
 }
+
 
 start()
--- layers/hacks.js
+++ layers/hacks.js
@@ -1,52 +1,51 @@
 module.exports.init = function init(TensorClass) {
   if (TensorClass.prototype.$) return TensorClass
 
-    TensorClass.prototype.$ = function $(selection) {
-      // translate from np to tfjs
-      // e.g.
-      // boxes[:, :, 1]
-      // becomes
-      // boxes.slice([0, 0, 2], [-1, -1, 1])
+  TensorClass.prototype.$ = function $(selection) {
+    // translate from np to tfjs
+    // e.g.
+    // boxes[:, :, 1]
+    // becomes
+    // boxes.slice([0, 0, 2], [-1, -1, 1])
 
-      // todo: memoize this
+    // todo: memoize this
 
-      var begin = []
+    var begin = []
 
-          var size = []
+    var size = []
 
-          var axes_to_squeeze = []
+    var axes_to_squeeze = []
 
-      selection.replace(/\s/g, '').split(',').forEach((slice, index) => {
-        if (/^[0-9]$/.test(slice)) {
-          let start = parseInt(slice, 10)
-          begin.push(start)
-          size.push(1)
-          axes_to_squeeze.push(index)
-        } else if (slice === ':') {
-          begin.push(0)
-          size.push(-1)
-        } else if (slice.startsWith(':')) {
-          // only support forward slices with no negative indices
-          let end = slice.split(':')[1]
-          begin.push(0)
-          size.push(parseInt(end, 10))
-        } else {
-          let [startS, endS] = slice.split(':')
-          let start = parseInt(startS, 10)
-          let end = parseInt(endS, 10)
-          let sliceSize = end - start
+    selection.replace(/\s/g, '').split(',').forEach((slice, index) => {
+      if (/^[0-9]+$/.test(slice)) {
+        let start = parseInt(slice, 10)
+        begin.push(start)
+        size.push(1)
+        axes_to_squeeze.push(index)
+      } else if (slice === ':') {
+        begin.push(0)
+        size.push(-1)
+      } else if (slice.startsWith(':')) {
+        // only support forward slices with no negative indices
+        let end = slice.split(':')[1]
+        begin.push(0)
+        size.push(parseInt(end, 10))
+      } else {
+        let [startS, endS] = slice.split(':')
+        let start = parseInt(startS, 10)
+        let end = parseInt(endS, 10)
+        let sliceSize = end - start
 
-          begin.push(start)
-          size.push(sliceSize)
-        }
-      })
-
-      // console.log(selection, begin, size)
-      let sliced = this.slice(begin, size)
-
-      if (axes_to_squeeze.length) {
-        return sliced.squeeze(axes_to_squeeze)
+        begin.push(start)
+        size.push(sliceSize)
       }
-      return sliced
+    })
+
+    let sliced = this.slice(begin, size)
+
+    if (axes_to_squeeze.length) {
+      return sliced.squeeze(axes_to_squeeze)
     }
+    return sliced
+  }
 }
