f0a0015c7e583bb465fed206ad0bcc7f960879cb
Eli Sakov <eli.sakov@hotmail.com>
2020-06-09 16:10:43 -0700

    add notes to readme readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-My in progress attempt to port EfficientDet from https://github.com/xuannianz/EfficientDet to run in tensorflow js.
+# EfficientDet JS
+
+## A live webcam demo of (EfficientDet)[https://github.com/google/automl/tree/master/efficientdet] 
+
+
+Commands to create the model. Requires that you get (automl)[https://github.com/google/automl/tree/master/efficientdet] running.
+
+1. downlaod the checkpoint
+
+    wget https://storage.googleapis.com/cloud-tpu-checkpoints/efficientdet/coco/efficientdet-d0.tar.gz
+
+2. export the saved model
+
+    python3 model_inspect.py --runmode=saved_model --model_name=efficientdet-d0 --ckpt_path=efficientdet-d0 
+
+3. Export to tensorflow.js.
+
+    tensorflowjs_converter --input_format=tf_saved_model --signature_name=serving_default --output_format=tfjs_graph_model  /tmp/saved_model/ websaved-d0 
+
+4. Copy to dist
+
+    cp -r websaved-d0 dist/d0 
+
+
+### Misc.
+
+This repository contains some tf.js keras layers, operations, and initializers that are not being used right now. This repo was originally a port of (this keras implementation)[https://github.com/xuannianz/EfficientDet] to JS before google released the automl repository with an official python implementation that can be exported as savedmodel.
--- index.ts
+++ index.ts
@@ -3,28 +3,17 @@ import 'babel-polyfill';
 import * as tf from '@tensorflow/tfjs';
 import { Tensor3D } from '@tensorflow/tfjs';
 
-import { PriorProbability } from './initializers';
-import { ClipBoxes } from './layers/ClipBoxes';
-import { FilterDetections } from './layers/FilterDetections';
-import { RegressBoxes } from './layers/RegressBoxes';
-import { Swish } from './layers/Sigmoids';
-import { wBiFPNAdd } from './layers/wBiFPNAddLayer';
 import { drawBoxes } from './drawBoxes';
 
 window.tf = tf
 
-// tf.enableDebugMode()
 tf.enableProdMode()
 tf.setBackend('webgl')
 
-tf.serialization.registerClass(Swish);
-tf.serialization.registerClass(RegressBoxes);
-tf.serialization.registerClass(wBiFPNAdd);
-tf.serialization.registerClass(PriorProbability)
-tf.serialization.registerClass(ClipBoxes)
-tf.serialization.registerClass(FilterDetections)
+const targetCanvas = document.createElement("canvas")
+targetCanvas.height = targetCanvas.width = 512
 
-const size = 256;
+const size = 512;
 
 const camConfig = {
   facingMode: 'user',
@@ -34,26 +23,34 @@ const camConfig = {
 }
 
 const videoElement = <HTMLVideoElement>document.getElementById('video');
-const canvasElement = <HTMLCanvasElement>document.getElementById('canvas');
-const testCanvasElement = <HTMLCanvasElement>document.getElementById('testcanvas');
+const liveCanvasElement = <HTMLCanvasElement>document.getElementById('canvas');
 
-testCanvasElement.width = canvasElement.width = size;
-testCanvasElement.height = canvasElement.height = size;
-
+// Labels for the COCO dataset
 const labels = {
-  15: "person",
-  2: "bike"
+  1: 'person', 2: 'bicycle', 3: 'car', 4: 'motorcycle', 5: 'airplane',
+  6: 'bus', 7: 'train', 8: 'truck', 9: 'boat', 10: 'traffic light',
+  11: 'fire hydrant', 13: 'stop sign', 14: 'parking meter', 15: 'bench',
+  16: 'bird', 17: 'cat', 18: 'dog', 19: 'horse', 20: 'sheep', 21: 'cow',
+  22: 'elephant', 23: 'bear', 24: 'zebra', 25: 'giraffe', 27: 'backpack',
+  28: 'umbrella', 31: 'handbag', 32: 'tie', 33: 'suitcase', 34: 'frisbee',
+  35: 'skis', 36: 'snowboard', 37: 'sports ball', 38: 'kite',
+  39: 'baseball bat', 40: 'baseball glove', 41: 'skateboard', 42: 'surfboard',
+  43: 'tennis racket', 44: 'bottle', 46: 'wine glass', 47: 'cup', 48: 'fork',
+  49: 'knife', 50: 'spoon', 51: 'bowl', 52: 'banana', 53: 'apple',
+  54: 'sandwich', 55: 'orange', 56: 'broccoli', 57: 'carrot', 58: 'hot dog',
+  59: 'pizza', 60: 'donut', 61: 'cake', 62: 'chair', 63: 'couch',
+  64: 'potted plant', 65: 'bed', 67: 'dining table', 70: 'toilet', 72: 'tv',
+  73: 'laptop', 74: 'mouse', 75: 'remote', 76: 'keyboard', 77: 'cell phone',
+  78: 'microwave', 79: 'oven', 80: 'toaster', 81: 'sink', 82: 'refrigerator',
+  84: 'book', 85: 'clock', 86: 'vase', 87: 'scissors', 88: 'teddy bear',
+  89: 'hair drier', 90: 'toothbrush',
 }
-
 const camera = tf.data.webcam(videoElement, camConfig);
 
 async function capturePhoto(): Promise<Tensor3D> {
   let cam = await camera;
   let img = await cam.capture();
-
-  let scaledImage = img.div(tf.scalar(255)) as Tensor3D
-
-  return scaledImage
+  return img as Tensor3D
 }
 
 
@@ -62,104 +59,52 @@ const timeEl = document.getElementById("time")
 async function loop(model) {
   let start = Date.now()
 
-  let scaledImage = await capturePhoto()
+  let img = await capturePhoto()
+  let originalImg = img
 
-  // let scaledImage = await capturePhoto() as Tensor3D
+  await tf.browser.toPixels(img, targetCanvas)
+
+  img = tf.browser.fromPixels(targetCanvas)
+  let scaledImage = tf.cast(img, "int32") as Tensor3D
   let batch = scaledImage.expandDims()
 
-  // https://github.com/tensorflow/tfjs/blob/fe4627f11effdff3b329920eae57a4c4b1e4c67c/tfjs-core/src/util.ts#L423
-
   console.time("Prediction")
-  let p = await model.executeAsync([batch])
+  let p = await model.executeAsync({ image_arrays: batch })
   console.timeEnd("Prediction")
 
-  // p has shape [batch_size, detections, predictions]
-  // detections contains [image_id, y, x, height, width, score, class]
-
-
-  let [predScores, predBoxes, predClasses, _] = await Promise.all([
-    p.$(":,:,5").data(),
-    p.$(":,:,1:5").data(),
-    p.$(":,:,6").data(),
-    tf.browser.toPixels(scaledImage, canvasElement)
-  ])
-
   let boxes = []
+  let predictions = p.arraySync()[0]
 
-  for (let i = 0; i < predScores.length; i++) {
-    let bbox = predBoxes.slice(i * 4, i * 4 + 4)
-    let [y, x, height, width] = bbox
-
-    if (predScores[i] < .7) continue
-    if (labels[predClasses[i]] === undefined) continue
-
+  predictions.forEach(out => {
+    let [image_id, y, x, ymax, xmax, score, _class] = out
+    if (score < .1) return
     boxes.push({
-      bbox: [y, x, height, width],
-      class: labels[predClasses[i]],
-      score: predScores[i]
+      bbox: {
+        x: x,
+        y: y,
+        width: xmax - x,
+        height: ymax - y
+      },
+      "class": labels[_class],
+      score
     })
-  }
+  });
 
-  drawBoxes(boxes, canvasElement)
+  await tf.browser.toPixels(originalImg, liveCanvasElement)
+
+  drawBoxes(boxes, liveCanvasElement)
 
   timeEl.innerText = Date.now() - start + "ms"
   loop(model)
 }
 
-async function test(model) {
-  // @TODO: This needs to use tf.tidy
-
-  const image = <HTMLImageElement>document.getElementById("img")
-  let img = tf.browser.fromPixels(image)
-  img = tf.image.resizeBilinear(img, [size, size])
-
-  let scaledImage = img.div(tf.scalar(255)) as Tensor3D
-
-  let batch = scaledImage.expandDims()
-
-  // https://github.com/tensorflow/tfjs/blob/fe4627f11effdff3b329920eae57a4c4b1e4c67c/tfjs-core/src/util.ts#L423
-
-  console.time("Prediction")
-  let p = await model.executeAsync([batch])
-  console.timeEnd("Prediction")
-
-  // p has shape [batch_size, detections, predictions]
-  // detections contains [image_id, y, x, height, width, score, class]
-
-  let predScores = tf.mul(2, p.$(":,:,5")).dataSync()
-  let predBoxes = p.$(":,:,1:5").dataSync()
-  let predClasses = p.$(":,:,6").dataSync()
-
-
-  await tf.browser.toPixels(scaledImage, testCanvasElement)
-
-  let boxes = []
-
-  for (let i = 0; i < predScores.length; i++) {
-    let bbox = predBoxes.slice(i * 4, i * 4 + 4)
-    let [y, x, height, width] = bbox
-
-    boxes.push({
-      bbox: [x, y, x + width, y + height],
-      class: labels[predClasses[i]],
-      score: predScores[i]
-    })
-  }
-
-  drawBoxes(boxes, testCanvasElement)
-}
-
-
 async function start() {
   await tf.ready()
 
-  let model = await tf.loadGraphModel('/web/model.json')
+  let model = await tf.loadGraphModel('d0/model.json')
+  window.model = model // to explore in the console
 
-  window.model = model
-
-  await test(model)
   await loop(model)
-
 }
 
 
