ff18736e1263ad7ab1033515d91b5950789e1e40
Eli Sakov <eli.sakov@hotmail.com>
2020-11-10 18:56:57 -0800

    increase test coverage

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,24 @@
 const EfficientDet = require("../build/index.js").default
+const tf = require("@tensorflow/tfjs")
 
 test("Test EfficientDet Constructor ", () => {
     const model = new EfficientDet()
     expect(model).toBeInstanceOf(EfficientDet)
+})
+
+describe("EfficientDet", () => {
+    const model = new EfficientDet()
+
+    test("Test EfficientDet Load ", done => {
+        model.load().then(done)
+    })
+
+    test("Test EfficientDet predict", async done => {
+        await model.load()
+
+        await model.predict(tf.ones([512, 512, 3]))
+
+        done()
+    })
 
 })
