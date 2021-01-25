5b565d3ce0d801f1813951f360aed3f0248615d5
Eli <eli@pop-os.localdomain>
2021-01-24 22:23:18 -0800

    cleanup old files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-My in progress attempt to port EfficientDet from https://github.com/xuannianz/EfficientDet to run in tensorflow js.
+# A live machine vision demo
--- layers/hacks.d.ts
+++
@@ -1,7 +0,0 @@
-import {Tensor} from '@tensorflow/tfjs';
-
-declare interface EasyTensor extends Tensor {
-  $(String): EasyTensor
-}
-
-export function init(Class): EasyTensor
